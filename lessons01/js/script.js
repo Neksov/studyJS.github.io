9e35644f2e96876bf153159bc7ea083ff36b4468
Nekrasov Vadim <n2v@list.ru>
2020-10-09 11:58:22 +0300

    add new work lessons01

---
+++ lessons01/js/script.js
@@ -0,0 +1,9 @@
+const money = '$';
+let income = 100000;
+let addExpenses = 70000;
+let deposit = 30000;
+const mission = 'Отпуск';
+const period = 12;
+
+// alert('Привет мир!');
+console.log('Сумма которую мы сможем отложить на отпуск за 12 месяцев: ' + (period * deposit + money));
