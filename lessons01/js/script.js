a1c8f0f2c845957f3c28e5675a2fe7b84bdd87f7
Nekrasov Vadim <n2v@list.ru>
2020-10-09 12:28:01 +0300

    добавил параметры к body и поменял js

--- lessons01/js/script.js
+++ lessons01/js/script.js
@@ -3,7 +3,7 @@ let income = 100000;
 let addExpenses = 70000;
 let deposit = 30000;
 const mission = 'Отпуск';
-const period = 12;
+const period = 24;
 
 // alert('Привет мир!');
 console.log('Сумма которую мы сможем отложить на отпуск за 12 месяцев: ' + (period * deposit + money));
