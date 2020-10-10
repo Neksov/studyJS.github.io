6b5c9a077f59c551381cce982ff35b3e6b7d6e70
Nekrasov Vadim <n2v@list.ru>
2020-10-10 17:39:14 +0300

    Добавил вывод двух параметров (period and mission), плюс в массив вывел слова в нижнем регистре

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 const money = 100000;
 let income = 'фриланс' ;
-let addExpenses = 'Продукты, ОДЕЖДА, Спортзал, ВЫХОДНЫЕ';
 let deposit = true;
+let addExpenses = 'Продукты, ОДЕЖДА, Спортзал, ВЫХОДНЫЕ';
 const mission = 500000;
 const period = 12;
-let budgetDay =money/30;
+let budgetDay = money/30;
 
 
 console.log('Тип данных параметра money: ' + typeof money);
 console.log('Тип данных параметра income: ' + typeof income);
 console.log('Тип данных параметра deposit: ' + typeof deposit);
-console.log('addExpenses содержит: ' + addExpenses.length + ' символов');
-console.log('То на что уходят наши деньги: ' +  addExpenses.toLowerCase());
-console.log(addExpenses.split(','));
-console.log ('Наш дневной бюджет: '+ budgetDay + ' рублей' );
+
+console.log('addExpenses содержит: ' + addExpenses.length + ' символов.');
+
+console.log('Период равен ' +  period + ' месяцам.' );
+console.log('Цель заработать ' +  mission + ' рублей.');
+
+let arr = 'То на что уходят наши деньги: ' +  addExpenses.toLowerCase();
+console.log(arr);
+console.log(arr.split(','));
+console.log ('Наш дневной бюджет: '+ budgetDay + ' рублей.' );
