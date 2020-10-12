a61f5846480f1f6819306c4a8491e285b8c373c5
Nekrasov Vadim <n2v@list.ru>
2020-10-13 00:18:44 +0300

    fix: Исправил условие if

--- js/hard.js
+++ js/hard.js
@@ -1,18 +1,2 @@
-function multiplication(num) {
-  let numString = num.toString(); //записываем значения переменной в строку
-  let sum = 1;
-  for (var i = 0; i < numString.length; i++) { //проходим по циклу пока не закончятся значения, перемножая каждый их по индексу между ссобой 
-    sum = sum * numString[i]; 
-  }
-  console.log('Результат числа которое ввели : ' + sum);
-
-  var power = sum ** 3;// возводим полученное значение в степень 3
-  console.log ('Полученное число возведенное в степень: ' + power);
-
-  var str = power.toString();
-  console.log('Покажем только первые 3 цифры нашего числа: ' + str.slice(0,2));
-};
-multiplication(266219);
-
 
 
--- js/script.js
+++ js/script.js
@@ -1,32 +1,24 @@
 const money = prompt('Ваш месячный доход?');
 console.log(typeof money)
-// console.log('Мой месячный доход: ' + money);
 
 let income = 'фриланс' ;
 console.log(typeof income);
 
 let deposit = confirm('Есть ли у вас депозит в банке? Если есть нажмите - ОК, если нет, то нажмите ОТМЕНА');
-console.log(typeof deposit)
-// console.log('Депозит в банке: ' +  Boolean(deposit));
-
-let expenses1 = prompt('Введите обязательную статью расходов?');
-// console.log('Cтатья расходов: ' + expenses1);
-
-let expenses2 = prompt('Введите обязательную статью расходов?');
-// console.log('Cтатья расходов: ' + expenses2);
-
-let amount1 = +prompt('Во сколько это обойдется?');
-// console.log('Сумма: ' + amount1);
-
-let amount2 = +prompt('Во сколько это обойдется?');
-// console.log('Сумма: ' + amount2);
+console.log(typeof deposit);
 
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 console.log('addExpenses содержит: ' + addExpenses.length + ' символов.');
- let arr = addExpenses.toLowerCase();
- console.log(arr.split(','));
+let arr = addExpenses.toLowerCase();
+console.log(arr.split(','));
 
-let budgetMonth = amount1+ amount2;
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = money-(amount1 + amount2);
 console.log('Бюджет на месяц ' + budgetMonth);
 
 const mission = 500000;
@@ -38,14 +30,15 @@ console.log('Цель будет достигнута за ' +  Math.round(month
 const period = 12;
 console.log('Период равен ' +  period + ' месяцам.' );
 
-let budgetDay =parseInt( budgetMonth/30);
+let budgetDay = +parseInt( budgetMonth/30);
 console.log ('Наш дневной бюджет: '+ budgetDay + ' рублей.' );
 
 if(budgetDay >= 1200){
   console.log('У вас высокий уровень дохода');
-}else if(600 <= budgetDay >= 600){
+}else if(budgetDay >= 600 && budgetDay <= 1200 ){
   console.log('У вас средний уровень дохода');
-}else if(600 <= budgetDay >= 0){
+}else if(budgetDay >= 0 && budgetDay <= 600){
   console.log('К сожалению у вас уровень дохода ниже среднего');
-}else(budgetDay<0)
+}else {
 console.log('Что то пошло не так');
+}
