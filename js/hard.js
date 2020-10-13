f283c8416863e2fbb0054e9689cd38fdc164da0b
Nekrasov Vadim <n2v@list.ru>
2020-10-13 23:39:22 +0300

    feat: Добавил условие if

--- js/hard.js
+++ js/hard.js
@@ -1,14 +1,22 @@
 function table (data) {
+  
   //проверка строки, является ли она строка с помощью операторов typeof и instanceof
-  if(typeof data === 'string' || data instanceof  String){
+  if(typeof data === 'string' || data instanceof  String ){
+
     //удаляем пробелы в начале и в конце с помощью метода trim()
     let aftertableLength = data.length;
     data = data.trim();
     let befortableLength = data.length;
     console.log("Длина строки до: " + aftertableLength);     
-    console.log("Длина строки после: " + befortableLength);  
+    console.log("Длина строки после: " + befortableLength); 
   }else 
   return('Не строка');
+
+//Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)
+  if(data.length > 30) {
+    data = data.substr(0, 30) + '...';
+    console.log(data);
+  }  
 };
-console.log(table(' 243   '));
+console.log(table('    12343123456789087654321345678987654321      '));
 
