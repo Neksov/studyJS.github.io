6cfb819b4a0498e591bb810e4c96b3b0b6e16139
Nekrasov Vadim <n2v@list.ru>
2020-10-21 01:35:06 +0300

    feat: Добавил валидацию

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let isNumber = function(n){ //проверка входящих prompt
 let money ,
     start = function(){//проверка входящих данных
       do { //интвертируем с помощью оператора !- отрицание
-        money = prompt('Ваш месячный доход?');
+        money = prompt('Ваш месячный доход?', 50000);
       }while (!isNumber(money))
     };
     start();
@@ -17,16 +17,41 @@ let appData = {// создали обьект со всеми переменны
   expenses: {}, // дополнительные расходы
   addExpenses: [], // возможные расходы
   deposit: false, 
+  percentDeposit: 0,
+  moneyDeposit: 0,
   mission: 50000,
-  period: 12,
+  period: 3,
   asking: function(){ // методы для вопросов
-    let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+    if(confirm('Если у Вас дополнительный источник заработка?')){
+      let itemIncome,
+      cashIncome=0;
+      do{
+        itemIncome = prompt('Какой у Вас дополнительный зарабаток?', 'Таксую');
+      }while(isNumber(itemIncome))
+
+      do{
+        cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?',  10000);
+      }while(!isNumber(cashIncome))
+    }
+    let addExpenses;
+        do{
+          addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+        }while(isNumber(addExpenses));
+
         appData.addExpenses = addExpenses.toLowerCase().split(',');
         appData.deposit = confirm('Есть ли у вас депозит в банке? Если есть нажмите - ОК, если нет, то нажмите ОТМЕНА');
 
+
+
         for(let i = 0; i < 2; i ++){
-          let b = 0,
-                a = prompt('Введите обязательную статью расходов?');
+          let b = 0;
+          let a;
+
+          do{
+            a = prompt('Введите обязательную статью расходов?');
+          }while(isNumber(a))
+
           do{
             b = +prompt('Во сколько это обойдется?');
           }while(!isNumber(b) || b === 0 )
@@ -36,7 +61,7 @@ let appData = {// создали обьект со всеми переменны
   },
   budget: money,
   budgetDay: 0,
-  budgetMonth: 0,
+  budgetMonth: 0, //сумма за месяц
   expensesMonth: 0,
   
   getExpensesMonth: function(){ //Функция возвращает сумму всех обязательных расходов за месяц
@@ -67,13 +92,28 @@ let appData = {// создали обьект со всеми переменны
     }else (appData.budgetDay >= 0 && appData.budgetDay <= 599)
       return('К сожалению у вас уровень дохода ниже среднего');
   },
+
+  getInfoDeposit: function(){
+    if(appData.deposit){
+      do{
+          appData.percentDeposit = prompt('Какой годовой процент?', '10');
+        }while(!isNumber(appData.percentDeposit))
+
+      do{
+          appData.moneyDeposit = prompt('Какая сумма заложена?', 10000);
+        }while(!isNumber(appData.moneyDeposit))
+    }
+  },
+
+  calcSavedMoney: function(){
+    return appData.budgetMonth * appData.period;
+  },
 };
 
 appData.asking();
+appData.getInfoDeposit();
 let expensesAmount = appData.getExpensesMonth();
-
 console.log('Наши расходы: ' + expensesAmount);
-
 appData.budgetMonth = appData.getBudget(), //присваеваем значени месячного накопления
 appData.budgetDay = +parseInt( appData.budgetMonth/30);
 
@@ -84,3 +124,13 @@ console.log('Наша программа включает в себя данны
 for(let key in appData) {
   console.log('Свойства: ' + key + ' Значения: ' + appData[key]);
 };
+
+
+const toTitleCase = (phrase) => {// выводим масив с каждым словом с большой буквы,слова разделены запятой и пробелом
+  return phrase
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(', ');
+};
+
+let result = toTitleCase(appData.addExpenses);
+console.log(result);
