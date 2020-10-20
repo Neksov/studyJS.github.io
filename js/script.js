0f53cbf3ea5d0f717f43e9820a2b83a2bc836c54
Nekrasov Vadim <n2v@list.ru>
2020-10-20 12:20:29 +0300

    feat: Реализовал вывод дней недели

--- js/script.js
+++ js/script.js
@@ -1,86 +1,25 @@
 'use strict';
 
-let isNumber = function(n){ //проверка входящих prompt
-  return !isNaN(parseFloat(n)) && isFinite(n)
-};
-let money ,
-    start = function(){//проверка входящих данных
-      do { //интвертируем с помощью оператора !- отрицание
-        money = prompt('Ваш месячный доход?');
-      }while (!isNumber(money))
-    };
-    start();
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];// массив 
+const day = document.getElementById('fordays'); // Получаем div куда будем вставлять дни недели
+const today = new Date(); // создаем обьект
 
-let appData = {// создали обьект со всеми переменными
-  income: {}, // доп доходы
-  addIncome: [], // дополнительные доходы
-  expenses: {}, // дополнительные расходы
-  addExpenses: [], // возможные расходы
-  deposit: false, 
-  mission: 50000,
-  period: 12,
-  asking: function(){ // методы для вопросов
-    let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-        appData.addExpenses = addExpenses.toLowerCase().split(',');
-        appData.deposit = confirm('Есть ли у вас депозит в банке? Если есть нажмите - ОК, если нет, то нажмите ОТМЕНА');
-
-        for(let i = 0; i < 2; i ++){
-          let b = 0,
-                a = prompt('Введите обязательную статью расходов?');
-          do{
-            b = +prompt('Во сколько это обойдется?');
-          }while(!isNumber(b) || b === 0 )
-          appData.expenses[a] = b;
-        };
-        return appData.expenses;
-  },
-  budget: money,
-  budgetDay: 0,
-  budgetMonth: 0,
-  expensesMonth: 0,
-  
-  getExpensesMonth: function(){ //Функция возвращает сумму всех обязательных расходов за месяц
-    for(let key in appData.expenses) {
-      appData.expensesMonth += appData.expenses[key];
+const days = function(){
+  week.forEach(function(item, i, week){ //перебираем массив 
+    let newdiv = document.createElement('div'); //создаем переменную и туда помешаем новый элемент с заданным тегом для каждого дня недели
+    if(i === today.getDay()-1){//проверка дня недели, -1 для массива так как он начинается с 0. getDay()-метод для определения дня недели
+      // console.log(today.getDay());
+      newdiv.classList.add('today'); //делаем текущий день жирным исходя из свойства, добавляя класс.
     }
-    return appData.expensesMonth;
-  },
 
-  getBudget: function(){ //Функция возвращает накопления за месяц
-    return money-expensesAmount;
-  },
-
-  getTargetMonth: function () { //Подсчитывает за какой период будет достигнута цель
-    let a = Math.round(appData.mission/appData.budgetMonth);
-    if(a < 0){
-      console.log('Цель не будет достигнута');
-    }else
-      console.log('Цель будет достигнута за ' + a + ' месяцев');
-    return(a)
-  },
-
-  getStatusIncome: function(){ //проверка уровня дохода
-    if(appData.budgetDay >= 1200){
-      return('У вас высокий уровень дохода');
-    }else if(appData.budgetDay >= 600 && appData.budgetDay <= 1199 ){
-      return('У вас средний уровень дохода');
-    }else (appData.budgetDay >= 0 && appData.budgetDay <= 599)
-      return('К сожалению у вас уровень дохода ниже среднего');
-  },
+    if(item == 'Суббота' || item == 'Воскресенье'){
+      newdiv.classList.add('italic'); //делаем текущий день курсивом исходя из свойства, добавляя класс.
+      newdiv.textContent = week[i]; //вставляем текст
+    }else{
+      newdiv.textContent = week[i]; //вставляем текст
+    }
+    day.appendChild(newdiv);// вставляем в наш div обновленные div
+  });
 };
 
-appData.asking();
-let expensesAmount = appData.getExpensesMonth();
-
-console.log('Наши расходы: ' + expensesAmount);
-
-appData.budgetMonth = appData.getBudget(), //присваеваем значени месячного накопления
-appData.budgetDay = +parseInt( appData.budgetMonth/30);
-
-appData.getTargetMonth();
-console.log(appData.getStatusIncome());
-
-console.log('Наша программа включает в себя данные:');
-for(let key in appData) {
-  console.log('Свойства: ' + key + ' Значения: ' + appData[key]);
-};
+days();
